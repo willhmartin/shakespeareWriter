c5d7abf8b9b4b9e9aca28f4fba32572eecf34821
Will Martin <whmartinwork@outlook.com>
2022-11-23 00:17:15 +0200

    move adjectives to lists, function to split string and return ASCII array, getBaseLog function

--- src/index.js
+++ src/index.js
@@ -1,109 +1,26 @@
 #!/usr/bin/env node
 import { createInterface } from "readline";
 import Sentencer from "sentencer";
+import { positiveAdjectives, negativeAdjectives } from "./lists.js";
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const positiveNoun = "lord";
-
-const negativeNoun = "peasant";
-
-let codeString = [];
+let ASCIIString = [];
 let adjectiveArray = [];
 let adjectiveString = "";
 
+// return the base log of a number, in this case 2 ** n
 function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
 }
 
-// rl.question("How many adjectives? > ", function (string) {
-//   for (let i = 0; i < Number(string); i += 1) {
-//     adjectiveArray.push(" {{ adjective }}");
-//   }
-
-//   console.log(Sentencer.make(`You are a${adjectiveArray} ${negativeNoun}.`));
-// });
-
 Sentencer.configure({
   nounList: [],
 
-  // adjectiveList: [
-  //   "bad",
-  //   "cowardly",
-  //   "cursed",
-  //   "damned",
-  //   "dirty",
-  //   "disgusting",
-  //   "distasteful",
-  //   "dusty",
-  //   "evil",
-  //   "fat",
-  //   "fat-kidneyed",
-  //   "fatherless",
-  //   "foul",
-  //   "hairy",
-  //   "half-witted",
-  //   "horrible",
-  //   "horrid",
-  //   "infected",
-  //   "lying",
-  //   "miserable",
-  //   "misused",
-  //   "oozing",
-  //   "rotten",
-  //   "smelly",
-  //   "snotty",
-  //   "sorry",
-  //   "stinking",
-  //   "stuffed",
-  //   "stupid",
-  //   "vile",
-  //   "villainous",
-  //   "worried",
-  // ],
-  // amazing beautiful blossoming bold brave charming clearest cunning cute delicious embroidered fair fine gentle golden good handsome happy healthy honest lovely loving mighty noble peaceful pretty prompt proud reddest rich smooth sunny sweet sweetest trustworthy warm
-
-  adjectiveList: [
-    "amazing",
-    "beautiful",
-    "blossoming",
-    "bold",
-    "brave",
-    "charming",
-    "clearest",
-    "cunning",
-    "cute",
-    "delicious",
-    "embroidered",
-    "fair",
-    "fine",
-    "gentle",
-    "golden",
-    "good",
-    "handsome",
-    "happy",
-    "healthy",
-    "honest",
-    "lovely",
-    "loving",
-    "mighty",
-    "noble",
-    "peaceful",
-    "pretty",
-    "prompt",
-    "proud",
-    "reddest",
-    "rich",
-    "smooth",
-    "sunny",
-    "sweet",
-    "sweetest",
-    "trustworthy",
-    "warm",
-  ],
+  adjectiveList: positiveAdjectives,
 
   actions: {
     my_action: function () {
@@ -111,18 +28,19 @@ Sentencer.configure({
     },
   },
 });
-console.log(true);
+
+function splitStringAndReturnASCII(string) {
+  string.split("").forEach(s => {
+    ASCIIString.push(s.charCodeAt(0));
+  });
+  console.log(ASCIIString);
+  ASCIIString.forEach(a => {
+    console.log(getBaseLog(2, a));
+  });
+}
+
 rl.question("> ", function (string) {
-  console.log(getBaseLog(2, string.charCodeAt(0)));
-  for (let i = 0; i < getBaseLog(2, string.charCodeAt(0)); i += 1) {
-    adjectiveArray.push(" {{ adjective }}");
-  }
-
-  const sentence = Sentencer.make(
-    `You are a${adjectiveArray} ${positiveNoun}. Speak your mind!`
-  );
-
-  console.log(sentence.replaceAll(",", ""));
+  splitStringAndReturnASCII(string);
 });
 
 rl.on("close", function () {
@@ -148,3 +66,13 @@ rl.on("close", function () {
 //     console.log(getBaseLog(2, c));
 //   });
 // });
+
+// for (let i = 0; i < getBaseLog(2, string.charCodeAt(0)); i += 1) {
+//   adjectiveArray.push(" {{ adjective }}");
+// }
+
+// const sentence = Sentencer.make(
+//   `You are a${adjectiveArray} ${positiveNoun}. Speak your mind!`
+// );
+
+// console.log(sentence.replaceAll(",", ""));
